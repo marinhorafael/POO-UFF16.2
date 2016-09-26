ceedf701164121c8c9bd7128acac27049848d567
MD10 <md10@curso.infnet.edu.br>
2016-09-26 16:17:28 -0300

    show

---
+++ Calculadora/src/calculadora/Leitura.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+import java.util.Scanner;
+
+/**
+ *
+ * @author md10
+ */
+public class Leitura {
+    Scanner teclado = new Scanner(System.in);
+    
+    
+}
---
+++ Calculadora/src/calculadora/Menu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+/**
+ *
+ * @author marinhorafael
+ */
+public class Menu {
+    
+    public static void chamarMenu(double n){
+        
+        System.out.println("Estado de memória: "+ n);
+        System.out.println("(1) Somar");
+        System.out.println("(2) Subtrair");
+        System.out.println("(3) Multiplicar");
+        System.out.println("(4) Dividir");
+        System.out.println("(5) Limpar memória");
+        System.out.println("(6) Sair do programa");
+        
+    }
+    
+}
